0512ca63675737f5ba6aa3783c1d41cda46abfe6
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-03-30 10:19:50 +0500

    Milestone-1 List Structure completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import _ from 'lodash';
 import './style.css';
+import tasksToDo from './tasksArray.js';
 
- function component() {
-   const element = document.createElement('div');
+const taskContainer = document.getElementById('tasksList');
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+const displayTasks = () => {
+  tasksToDo.forEach((task) => {
+    const taskCard = document.createElement('div');
+    taskCard.classList = 'tasksContent';
+    taskCard.innerHTML = `<div class="taskText">
+                            <input type="checkbox">
+                            <p class="taskText">${task.description}</p>
+                          </div>
+                          <i class="fa-solid fa-trash-can" id="deleteTask"></i>`;
+    taskContainer.appendChild(taskCard);
+  });
+};
 
-   return element;
- }
-
- document.body.appendChild(component());
+displayTasks();
