0327a237c71431422b83f465437bdc5426012e11
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-03-30 11:57:21 +0500

    Corrected webconfig error

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
-import { resolve } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-export const entry = {
-  index: './src/index.js',
-};
-export const mode = 'none';
-export const devtool = 'inline-source-map';
-export const devServer = {
-  static: './dist',
-};
-export const plugins = [
-  new HtmlWebpackPlugin({
-    template: './src/index.html',
-  }),
-];
-export const output = {
-  filename: '[name].bundle.js',
-  path: resolve(__dirname, 'dist'),
-  clean: true,
-};
-export const optimization = {
-  runtimeChunk: 'single',
-};
-export const module = {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: 'asset/resource',
-    },
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  mode: 'none',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
   ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
